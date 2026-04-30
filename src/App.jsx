5129633371ebe7c46011f0d1c9991321c91ef405
Graf-Lev-V <legendarlev2004@gmail.com>
2026-04-30 16:23:02 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,55 +8,43 @@ const upgradesConfig = {
     nextCost: (level) => Math.floor(10 * (level + 1) ** 1.3),
     baseValue: 1,
     getValue: (level) => 1 + level * 1,
-    key: "clickPower", 
     title: "Click Power",
     description: "+1 coin per click",
-    nextLevel: (state) => ({...state, clickPower: state.clickPower + 1})
   },
   autoCoins: {
     nextCost: (level) => Math.floor(25 * (level + 1) ** 1.4),
     baseValue: 0,
     getValue: (level) => 0 + level * 1,
-    key: "autoCoins", 
     title: "Auto Coins",
     description: "+1 coin per second",
-    nextLevel: (state) => ({...state, autoCoins: state.autoCoins + 1})
   },
   coinMultiplier: {
     nextCost: (level) => Math.floor(70 * 1.7 ** level),
     baseValue: 1,
     getValue: (level) => 1 + level * 1,
-    key: "coinMultiplier",
     title: "Coin Multiplier",
     description: `x2 coins per click`,
-    nextLevel: (state) => ({...state, coinMultiplier: state.coinMultiplier + 1})
   },
   autoCoinsMultiplier: {
     nextCost: (level) => Math.floor(140 * 1.6 ** level),
     baseValue: 1,
     getValue: (level) => 1 + level * 1,
-    key: "autoCoinsMultiplier",
     title: "Auto Coins Multiplier",
     description: `x2 coins per second`,
-    nextLevel: (state) => ({...state, autoCoinsMultiplier: state.autoCoinsMultiplier + 1})
   },
   critChance: {
     nextCost: (level) => Math.floor(80 * 1.5 ** level),
     baseValue: 0,
     getValue: (level) => 0 + level * 0.05,
-    key: "critChance",
     title: "Crit Chance",
     description: "+5% chance coins multiplied per click",
-    nextLevel: (state) => ({...state, critChance: state.critChance + 1})
   },
   critPower: {
     nextCost: (level) => Math.floor(120 * 1.4 ** level),
     baseValue: 2,
     getValue: (level) => 2 + level * 0.5,
-    key: "critPower",
     title: "Crit Power",
     description: "x2 coins multiplied per crit",
-    nextLevel: (state) => ({...state, critPower: state.critPower + 1})
   }
 }
 
@@ -75,36 +63,29 @@ export default function App() {
     autoCoinsMultiplier: 0,
     critChance: 0,
     critPower: 0
-  })
+  });
 
   //crit
   const [crit, setCrit] = useState(false);
 
-  //auto coins 
-  useEffect(() => {
-    const autoClick = setInterval(() => setCoins((prev) => 
-      prev + 
-      (upgradesConfig.autoCoins.getValue(upgradeLevel.autoCoins) * 
-      upgradesConfig.autoCoinsMultiplier.getValue(upgradeLevel.autoCoinsMultiplier))), 1000);
-    return () => clearInterval(autoClick);
-  }, [upgradeLevel.autoCoins, upgradeLevel.autoCoinsMultiplier]);
+  const values = {
+    clickPower: upgradesConfig.clickPower.getValue(upgradeLevel.clickPower),
+    autoCoins: upgradesConfig.autoCoins.getValue(upgradeLevel.autoCoins),
+    coinMultiplier: upgradesConfig.coinMultiplier.getValue(upgradeLevel.coinMultiplier),
+    autoCoinsMultiplier: upgradesConfig.autoCoinsMultiplier.getValue(upgradeLevel.autoCoinsMultiplier),
+    critChance: upgradesConfig.critChance.getValue(upgradeLevel.critChance),
+    critPower: upgradesConfig.critPower.getValue(upgradeLevel.critPower)
+  }
 
   //click
   function handleClick() {
-    if (Math.random() <= upgradesConfig.critChance.getValue(upgradeLevel.critChance)) {
+    if (Math.random() <= values.critChance) {
       setCrit(true);
-      setCoins((prev) => 
-        prev + 
-        (upgradesConfig.clickPower.getValue(upgradeLevel.clickPower) * 
-        upgradesConfig.coinMultiplier.getValue(upgradeLevel.coinMultiplier) * 
-        upgradesConfig.critPower.getValue(upgradeLevel.critPower)))
+      setCoins((prev) => prev + (values.clickPower * values.coinMultiplier * values.critPower))
     } 
     else  {
       setCrit(false);
-      setCoins((prev) => 
-        prev + 
-        (upgradesConfig.clickPower.getValue(upgradeLevel.clickPower) * 
-        upgradesConfig.coinMultiplier.getValue(upgradeLevel.coinMultiplier)))
+      setCoins((prev) => prev + (values.clickPower * values.coinMultiplier))
     }
   }
 
@@ -113,80 +94,68 @@ export default function App() {
     if (coins >= upgradesConfig[upgradeKey].nextCost(upgradeLevel[upgradeKey])) {
       if (count === "one") {
         setCoins((prev) => prev - upgradesConfig[upgradeKey].nextCost(upgradeLevel[upgradeKey]));
-        setUpgradeLevel(upgradesConfig[upgradeKey].nextLevel(upgradeLevel));
+        setUpgradeLevel((prev) => ({...prev, [upgradeKey]: prev[upgradeKey] + 1}));
       }
       if (count === "max") {
         let tempCoins = coins;
         let tempCost = upgradesConfig[upgradeKey].nextCost(upgradeLevel[upgradeKey]);
-        let tempLevel = {...upgradeLevel};
+        let tempLevel = upgradeLevel[upgradeKey];
         while (tempCost <= tempCoins) {
           tempCoins -= tempCost;
-          tempCost = upgradesConfig[upgradeKey].nextCost(tempLevel[upgradeKey]);
-          tempLevel = upgradesConfig[upgradeKey].nextLevel(tempLevel);
+          tempLevel += 1;
+          tempCost = upgradesConfig[upgradeKey].nextCost(tempLevel);
         }
         setCoins(tempCoins);
-        setUpgradeLevel(tempLevel);
+        setUpgradeLevel({...upgradeLevel, [upgradeKey]: tempLevel});
       }
     }
   }
 
-  //reset
-  function handleReset() {
-    localStorage.clear();
-    setCoins(0);
-    setUpgradeLevel({
-      clickPower: 0,
-      autoCoins: 0,
-      coinMultiplier: 0,
-      autoCoinsMultiplier: 0,
-      critChance: 0,
-      critPower: 0
-    });
-  }
-
-  //debug
-  function manyCoins() {
-    setCoins(1000000000000);
-  }
+  //auto coins 
+  useEffect(() => {
+    const autoClick = setInterval(() => setCoins((prev) => prev + (values.autoCoins * values.autoCoinsMultiplier)), 1000);
+    return () => clearInterval(autoClick);
+  }, [values.autoCoins, values.autoCoinsMultiplier]);
 
   //local storage
   useEffect(() => {
     localStorage.setItem("coins", coins);
     localStorage.setItem("upgradeLevel", JSON.stringify(upgradeLevel));
-  }, [coins, upgradeLevel])
+  }, [coins, upgradeLevel]);
 
   return (
     <>
       <ClickButton 
-      handleClick={handleClick} 
-      clickPower={upgradesConfig.clickPower.getValue(upgradeLevel.clickPower)} 
-      coinMultiplier={upgradesConfig.coinMultiplier.getValue(upgradeLevel.coinMultiplier)}
-      critPower={upgradesConfig.critPower.getValue(upgradeLevel.critPower)}
-      crit={crit}/>
+        handleClick={handleClick} 
+        clickPower={values.clickPower} 
+        coinMultiplier={values.coinMultiplier}
+        critPower={values.critPower}
+        crit={crit}
+      />
       <hr/>
       <p>Coins: {coins}</p>
       <hr/>
       <Stats 
-      coins={coins} 
-      clickPower={upgradesConfig.clickPower.getValue(upgradeLevel.clickPower)} 
-      autoCoins={upgradesConfig.autoCoins.getValue(upgradeLevel.autoCoins)}
-      coinMultiplier={upgradesConfig.coinMultiplier.getValue(upgradeLevel.coinMultiplier)}
-      autoCoinsMultiplier={upgradesConfig.autoCoinsMultiplier.getValue(upgradeLevel.autoCoinsMultiplier)}
-      critChance={upgradesConfig.critChance.getValue(upgradeLevel.critChance)}
-      critPower={upgradesConfig.critPower.getValue(upgradeLevel.critPower)}/>
+        coins={coins} 
+        clickPower={values.clickPower} 
+        autoCoins={values.autoCoins}
+        coinMultiplier={values.coinMultiplier}
+        autoCoinsMultiplier={values.autoCoinsMultiplier}
+        critChance={values.critChance}
+        critPower={values.critPower}
+      />
       <hr/>
-      {Object.values(upgradesConfig).map((upgrade) => <Shop 
-        key={upgrade.key}
+      {Object.entries(upgradesConfig).map((key, upgrade) => <Shop 
+        key={key}
         handleBuy={handleBuy}
-        upgradeCost={upgradesConfig[upgrade.key].nextCost(upgradeLevel[upgrade.key])}
+        upgradeCost={upgradesConfig[key].nextCost(upgradeLevel[key])}
         coins={coins}
         upgrade={upgrade}
         upgradeLevel={upgradeLevel}
         />
-      )}    
+      )}
       <hr/>
-      <button onClick={handleReset}>Reset</button>
-      <button onClick={manyCoins}>Give Coins</button>
+      
     </>
   )
 }
